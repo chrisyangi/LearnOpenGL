53fa6a4776f9658e4ab59ac90bda79842515ba02
Chris <yangsongyei@gmail.com>
2022-02-26 20:54:28 +0800

    spotlight（类似手电筒的光源）

--- color.fs
+++ color.fs
@@ -5,9 +5,6 @@ in vec3 Normal;
 in vec3 FragPos;
 in vec3 LightPos;
 in vec2 TexCoords;
-//uniform vec3 objectColor;
-//uniform vec3 lightColor;
-// uniform vec3 viewPos;
 
 struct Material{
     sampler2D diffuse;
@@ -17,31 +14,42 @@ struct Material{
 
 struct Light{
     vec3 position;
+    vec3 direction;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+    float cutOff;
+    float outerCutOff;
+    float constant;
+    float linear;
+    float quadratic;
 };
 uniform Light light;
 uniform Material material;
-uniform sampler2D emission;
 
 void main()
 {
-    vec3 norm = normalize(Normal);
+    
+    vec3 lightDir = normalize(light.position - FragPos);
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+    float theta     = dot(lightDir, normalize(-light.direction));
+    float epsilon   = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);   
+        vec3 norm = normalize(Normal);
 
-    vec3 ambient =light.ambient * vec3(texture(material.diffuse,TexCoords));
+        vec3 ambient =light.ambient * vec3(texture(material.diffuse,TexCoords)) * attenuation * intensity;
 
-    vec3 lightDir = normalize(LightPos - FragPos);
-    float diff = max(dot(lightDir , norm) , 0);
-    vec3 diffuse =light.diffuse * vec3(texture(material.diffuse,TexCoords));
+        float diff = max(dot(lightDir , norm) , 0);
+        vec3 diffuse =light.diffuse * vec3(texture(material.diffuse,TexCoords)) * attenuation;
 
-    vec3 viewDir = normalize(-FragPos);
-    vec3 reflectDir = reflect(-lightDir , norm);
-    float spec = pow(max(dot(viewDir , reflectDir) , 0.0f) , material.shininess);
-    vec3 specular = light.specular * spec * vec3(texture(material.specular,TexCoords));
+        vec3 viewDir = normalize(-FragPos);
+        vec3 reflectDir = reflect(-lightDir , norm);
+        float spec = pow(max(dot(viewDir , reflectDir) , 0.0f) , material.shininess);
+        vec3 specular = light.specular * spec * vec3(texture(material.specular,TexCoords)) * attenuation * intensity;
 
-    vec3 emission = vec3(texture(emission , TexCoords));
+        //vec3 emission = vec3(texture(emission , TexCoords));
 
-    vec3 result = ambient + specular + diffuse + emission;
-    FragColor = vec4(result , 1.0f);
+        vec3 result = ambient + specular + diffuse;
+        FragColor = vec4(result , 1.0f);
 }
--- main.cpp
+++ main.cpp
@@ -115,6 +115,18 @@ int main()
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
+    glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 
     unsigned int VBO , cubeVAO , lightcubeVAO;
     glGenVertexArrays(1, &cubeVAO);
@@ -140,17 +152,15 @@ int main()
     
     unsigned int diffuseMap = loadTexture("C:/Users/admin/Desktop/container2.png");
     unsigned int specularMap = loadTexture("C:/Users/admin/Desktop/container2_specular.png");
-    unsigned int emissionMap = loadTexture("C:/Users/admin/Desktop/matrix.jpg");
+
     lightingShader.use();
     lightingShader.setInt("material.diffuse", 0);
     lightingShader.setInt("material.specular", 1);
-    lightingShader.setInt("emission", 2);
+
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, diffuseMap);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, specularMap);
-    glActiveTexture(GL_TEXTURE2);
-    glBindTexture(GL_TEXTURE_2D, emissionMap);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -179,20 +189,29 @@ int main()
         lightingShader.setMat4("model", model);
         lightingShader.setMat4("view", view);
         lightingShader.setMat4("projection", projection);
-        lightingShader.setVec3("lightPos", lightPos.x , lightPos.y , lightPos.z);
-        lightingShader.setVec3("viewPos", camera.Position.x , camera.Position.y , camera.Position.z);
-        lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-        lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
-        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+        lightingShader.setVec3("light.position" ,lightPos.x , lightPos.y , lightPos.z);
+        lightingShader.setVec3("light.direction", camera.Front.x, camera.Front.y, camera.Front.z);
+        lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+        lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
         lightingShader.setFloat("material.shininess", 32.0f);
-        lightingShader.setVec3("material.emissionMap", 1.0f, 1.0f, 1.0f);
 
         lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
         lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setFloat("light.constant", 1.0f);
+        lightingShader.setFloat("light.linear", 0.09f);
+        lightingShader.setFloat("light.quadratic", 0.032f);
         glBindVertexArray(cubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
+        for (unsigned int i = 0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            lightingShader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
         
         lightCubeShader.use();
         lightCubeShader.setMat4("projection", projection);
@@ -203,7 +222,7 @@ int main()
         lightCubeShader.setMat4("model", model);
         glBindVertexArray(lightcubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
-
+        
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
